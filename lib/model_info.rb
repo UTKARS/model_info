1633040c40c4fa1e687d53394862b15374265cff
nitanshu verma <nitanshu@headerlabs.com>
2016-02-25 14:56:07 +0530

    json csv and xml downloads is added bootstrap removed

--- lib/model_info.rb
+++ lib/model_info.rb
@@ -1,11 +1,3 @@
 require "model_info/engine"
 module ModelInfo
-  mattr_accessor :download_pdf
-  @@download_pdf = false
-
-  mattr_accessor :download_docx
-  @@download_docx = false
-
-  mattr_accessor :download_excel
-  @@download_excel = false
 end
